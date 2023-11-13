484dc037dbc5ad3f4b00c99b0ce2b7ce13a21ae9
markus-sobotta <59049490+markus-sobotta@users.noreply.github.com>
2023-11-13 19:30:02 +0100

    Benutzer GAST

--- Beispiel-Download-aus-Genesis/testdownload.R
+++ Beispiel-Download-aus-Genesis/testdownload.R
@@ -1,10 +1,10 @@
 #Test Download ------
 
-user <- NULL
-pass <- NULL
+user <- "GAST"
+pass <- "GAST"
 EVAS <- "21111-01-03-4-B"
 
-source("download.R")
+source("Beispiel-Download-aus-Genesis/download.R")
 
 data <- downloadFlat(EVAS = EVAS, user = user, pass = pass, startyear = 2020, endyear = 2020)
 str(data)
