2f86396d95755a4f990af1d426f67ee5d85ac335
markus-sobotta <59049490+markus-sobotta@users.noreply.github.com>
2023-11-14 17:08:31 +0100

    update quiz, einfache Plotfunktion

--- Module/app2.R
+++ Module/app2.R
@@ -16,24 +16,6 @@ df <- tibble(
   Wert = c(1, 2, 3, 4, 5, 6)
 )
 
-
-dfalt <- tibble(
-  Bundesland = c("Sachsen", "Sachsen", "Sachsen", "Sachsen"),
-  Frage = c(
-    "Wie heißt die größte Stadt Sachsens?",
-    "Wie heißt der flächenmäßig größte Landkreis Sachsens?",
-    "Wie heißt die Landeshauptstadt Sachsens?",
-    "Wie heißt die östlichste Stadt Deutschlands?"
-  ),
-  Antwort1 = c("Leipzig, kreisfreie Stadt", "Bautzen", "Plauen", "Hannover"),
-  Antwort2 = c("Dresden, Landeshauptstadt", "Mittelsachsen", "Dresden", "Görlitz"),
-  Antwort3 = c("Zwickau", "Leipzig, Land", "Chemnitz", "Meißen"),
-  Istrichtig1 = c(TRUE, TRUE, FALSE, FALSE),
-  Istrichtig2 = c(FALSE, FALSE, TRUE, TRUE),
-  Istrichtig3 = c(FALSE, FALSE, FALSE, FALSE)
-)
-
-
 # TODO in utils.R verschieben
 # Funktionen -----
 ziehen <- function(df) {
@@ -78,8 +60,6 @@ fragenziehen <- function(df, themen, anzahl) {
     sample(anzahl, replace = FALSE) %>%
     return()
 }
-
-
 
 richtig <- modalDialog(
   title = "Korrekt",
@@ -205,10 +185,6 @@ server <- function(input, output, session) {
       })
       showNotification("Spiel beendet!")
 
-      # output$downloadcertificate <- downloadHandler(
-      #   filename = paste0("certificate", Sys.Date(), ".pdf"),
-      #   content = ,
-      # )
     }
   })
 
@@ -232,7 +208,20 @@ server <- function(input, output, session) {
     output$Chart <- renderUI({
       # TODO passenden Chart anzeigen
       tagList(
-        fluidRow(p(fragezeile() %>% select(Info) %>% pull())),
+        fluidRow(p(fragezeile() %>% select(Info) %>% pull())), #Info zur Statistik
+        fluidRow(
+          selectInput(inputId = "Jahr",
+                      label = "Jahr auswählen", 
+                      choices = df %>% filter(Statistik_Code == selectedfragen()[i()], 
+                                              is.na(Zwei_Code)) %>% 
+                                                select(Zeit) %>% 
+                                                unique() %>% 
+                                                pull()),
+          output$p <- renderPlot({
+            ggplot(df %>% filter(Statistik_Code == selectedfragen()[i()], Zeit == input$Jahr, is.na(Zwei_Code)),
+                   aes(x = AGS_Label, y = Wert)) + geom_col()+theme_bw()
+          }, res = 96)
+        ),
         fluidRow(
           actionButton(
             inputId = "naechsteFrage",
